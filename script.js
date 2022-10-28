af4eb1e386af60afabb3d5f9327fbeaf07fef5aa
Vbif322 <hxdfue@yandex.ru>
2022-10-28 18:22:51 +0300

    nice try to complete css...

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const btnAdd = document.querySelector('#add');
+const field = document.querySelector('.field');
 let catsName = ['Лунтик','Шпунтик'];
 let catsAge = [9,7];
 
-
+// for (let i in catsName){
+//     field.innerHTML+=`<div class="card">
+//                             <p class="card__name">${catsName[i]}</p>
+//                             <p class="card__age">${catsAge[i]}</p>
+//                             <p class="card__rate">★★★★★</p>
+//                             <img src="https://fikiwiki.com/uploads/posts/2022-02/1644927822_23-fikiwiki-com-p-smeshnie-kartinki-pro-kotyat-23.jpg"
+//                             class="card__foto">
+//                             <p class="card__cost">Примерная стоимость</p>
+//                             <button id="about">Подробнее</button>
+//                       </div>`
+// }
 
 
 // btnAdd.addEventListener('click', show())
