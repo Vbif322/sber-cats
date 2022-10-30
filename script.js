9892ed7518521904ad4a7954a69ffaf0bc235e3a
Vbif322 <hxdfue@yandex.ru>
2022-10-31 02:24:58 +0300

    form

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const btnAdd = document.querySelector('.btnAdd');
+const btnClose = document.querySelector('.close');
+const popup = document.getElementById('popup');
 const field = document.querySelector('.field');
+console.log(popup)
 let catsName = ['Шпунтик'];
 let catsAge = [7];
 const api = new Api();
@@ -14,19 +17,8 @@ const show = () => {
         })
 
     })
-    // for (let i in catsName){
-    //     field.innerHTML+=`<div class="card">
-    //                 <div class="card__info">
-    //                 <div class="card__name">${catsName[i]}</div>
-    //                 <div class="card__age">${catsAge[i]} лет</div>
-    //                 <div class="card__rate"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></div>
-    //             </div>
-    //             <img src="https://fikiwiki.com/uploads/posts/2022-02/1644927822_23-fikiwiki-com-p-smeshnie-kartinki-pro-kotyat-23.jpg"
-    //                 class="card__photo">
-    //             <button id="about">Подробнее</button>
-    //         </div>`
-    // }
 }
 show();
 
-// btnAdd.addEventListener('click', showPopup);
+btnClose.addEventListener('click', () => popup.className = 'popup-wrapper unactive')
+btnAdd.addEventListener('click', () => popup.className = 'popup-wrapper');
