47e591068c4f1abbf566d674890141914e60c2f7
Vbif322 <hxdfue@yandex.ru>
2022-10-31 18:54:44 +0300

    serialize form start

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const btnAdd = document.querySelector('.btnAdd');
 const btnClose = document.querySelector('.close');
+const btnSubmit = document.querySelector('.form__btn')
 const popup = document.getElementById('popup');
 const field = document.querySelector('.field');
+const formAdd = document.querySelector('#form');
 let ls = localStorage;
 let idArr = [];
 let catsName = [];
@@ -20,6 +22,24 @@ const show = () => {
     })
 }
 show();
+
+const serializeForm = (arr) =>{
+    const formData = {};
+
+    arr.forEach(input => {
+        if(input.type !== 'submit') {
+            formData[input.name] = input.value;
+        } else return;
+    })
+    console.log(formData);
+}
+
+const arrForm = [...formAdd.elements]
+serializeForm(arrForm)
+
+const submitFrom = () => {
+
+}
 
 btnClose.addEventListener('click', () => popup.className = 'popup-wrapper unactive')
 btnAdd.addEventListener('click', () => popup.className = 'popup-wrapper');
